36645983cbd43d11dbe895eb02e416cfde69d464
ben-jamming-reilly <benreilly0809@gmail.com>
2020-06-21 16:30:58 -0700

    a

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { PropTypes } from "react";
+//import { geolocated } from "react-geolocated";
+//import LocationProvider, { useLocationContext } from "react-geoloc";
+import useGeolocation from "react-hook-geolocation";
+import Geolocation from "react-geolocation";
 
 //import "./App.css";
 
-function App() {
-  return <div>Shit</div>;
-}
+const App = () => {
+  const geolocation = useGeolocation();
+  return (
+    <Geolocation
+      render={({
+        fetchingPosition,
+        position: { coords: { latitude, longitude } = {} } = {},
+        error,
+        getCurrentPosition,
+      }) => (
+        <div>
+          <button onClick={getCurrentPosition}>Get Position</button>
+          {error && <div>{error.message}</div>}
+          <pre>
+            latitude: {latitude}
+            longitude: {longitude}
+          </pre>
+        </div>
+      )}
+    />
+  );
+};
 
 export default App;
